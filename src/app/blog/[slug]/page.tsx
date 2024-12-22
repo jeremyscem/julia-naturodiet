7249d31ea0ef3e9e254675dd602a99ab02c56c50
Scemama Jeremy <scemama.jeremy@gmail.com>
2024-12-22 23:04:41 +0200

    fix

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,24 +1,25 @@
-export async function generateStaticParams() {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+// export async function generateStaticParams() {
+//   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+//   console.log("Base URL:", baseUrl);
 
-  // Ensure we only fetch from a valid URL
-  if (!baseUrl.startsWith("http")) {
-    throw new Error("Invalid NEXT_PUBLIC_BASE_URL");
-  }
+//   // Ensure we only fetch from a valid URL
+//   if (!baseUrl.startsWith("http")) {
+//     throw new Error("Invalid NEXT_PUBLIC_BASE_URL");
+//   }
 
-  const response = await fetch(`${baseUrl}/api/blog`);
-  if (!response.ok) {
-    throw new Error(`Failed to fetch blog data: ${response.statusText}`);
-  }
-  const posts = await response.json();
+//   const response = await fetch(`${baseUrl}/api/blog`);
+//   if (!response.ok) {
+//     throw new Error(`Failed to fetch blog data: ${response.statusText}`);
+//   }
+//   const posts = await response.json();
 
-  return posts["blogData"].map((post: any) => ({
-    slug: post.slug,
-  }));
-}
+//   return posts["blogData"].map((post: any) => ({
+//     slug: post.slug,
+//   }));
+// }
 
-export default function Page({ params }: { params: { slug: string } }) {
-  const { slug } = params;
+// export default function Page({ params }: { params: { slug: string } }) {
+//   const { slug } = params;
 
-  return <h1>{slug}</h1>;
-}
+//   return <h1>{slug}</h1>;
+// }
